3b4fb336e93e2d7f0bb37bf96491174e13f91fee
Dhiraj320 <95081385+Dhiraj320@users.noreply.github.com>
2022-08-31 04:45:02 +0530

    Update coin_data.dart

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,4 +1,4 @@
-//2. Import the required packages
+
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -29,16 +29,16 @@ const List<String> currenciesList = [
 const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
 
 const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
-const apiKey = ' 27D29E0B-5618-4A2C-8559-BBE6707E3A9E';
+const apiKey = 'A7AFC9CA-6777-484C-A75D-7A9050B127FC';
 
 class CoinData {
-  //TODO 3: Update getCoinData to take the selectedCurrency as an input.
+ 
   Future getCoinData(String selectedCurrency) async {
     Map<String, String> cryptoPrices = {};
     for (String crypto in cryptoList) {
-       //TODO 4: Update the URL to use the selectedCurrency input.
+  
       var url = Uri.parse(
-          "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey= 27D29E0B-5618-4A2C-8559-BBE6707E3A9E");
+          "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey= A7AFC9CA-6777-484C-A75D-7A9050B127FC");
       http.Response response = await http.get(url);
       if (response.statusCode == 200) {
         var decodedData = jsonDecode(response.body);
@@ -52,4 +52,4 @@ class CoinData {
     return cryptoPrices;
   }
 }
-//https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=4544B577-B95A-40CC-BC30-BA32BF7FBFB2
+
